1913387a1e967fb505d027f05d2e56afdf0ab509
yeyevtushenko <yehor803@gmail.com>
2024-01-03 14:56:25 +0200

    Реалізація переміщення диску

--- main.py
+++ main.py
@@ -6,5 +6,8 @@ class HanoiTower:
         self.auxiliary = "Вежа 2"
         self.moves_stack = []
 
+    def move_disk(self, from_tower, to_tower):
+        print(f"Перемістіть диск з {from_tower} на {to_tower}")
+
 
 
