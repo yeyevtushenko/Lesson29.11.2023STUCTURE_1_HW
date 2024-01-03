438e87e476b43681eee1cbaa034816bac8a83b36
yeyevtushenko <yehor803@gmail.com>
2024-01-03 14:55:52 +0200

    Ініціалізація класу

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+class HanoiTower:
+    def __init__(self, n):
+        self.n = n
+        self.source = "Вежа 1"
+        self.target = "Вежа 3"
+        self.auxiliary = "Вежа 2"
+        self.moves_stack = []
+
+
 
